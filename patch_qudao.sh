dcd0e4428090a0011288e9378a75210e540e60d5
夜刃 <75507531+dark-knifes@users.noreply.github.com>
2023-03-23 17:28:08 +0800

    Update patch_qudao.sh

--- patch_qudao.sh
+++ patch_qudao.sh
@@ -29,7 +29,7 @@ chmod +x apkeep
 download_azurlane () {
     if [ ! -f "AzurLane.apk" ]; then
     # 渠道服全都不是直链,不知道怎么搞了
-    url="https://downali.game.uc.cn/s1/2/10/20230213150150_blhx_uc_2022_11_02_18_24_01.apk?x-oss-process=udf/uc-apk,ZBHDhDR0LVBkTsK*wpLCng==afae37c2a88fd1ca&sh=10&sf=1831727323&vh=18330f93bd450707942ce0b882a0c6b2&cc=2521889677&did=217b729f8a4841dd81901816dfba551f"
+    url="https://appdl6-drcn.dbankcdn.cn/dl/appdl/application/apk/7e/7e70bd9184a643ddaa4fc6cb9403817c/com.bilibili.blhx.huawei.2211101822.apk"
     # 使用wget命令下载apk文件
     curl -o blhx.apk  $url
     fi
