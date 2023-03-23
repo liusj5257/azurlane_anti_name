efbd73acbba556349d24645afe779c77c4bc5790
liusj5257 <87109954+liusj5257@users.noreply.github.com>
2023-03-23 18:55:07 +0800

    test

---
+++ download.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Download apkeep
+get_artifact_download_url () {
+    # Usage: get_download_url <repo_name> <artifact_name> <file_type>
+    local api_url="https://api.github.com/repos/$1/releases/latest"
+    local result=$(curl $api_url | jq ".assets[] | select(.name | contains(\"$2\") and contains(\"$3\") and (contains(\".sig\") | not)) | .browser_download_url")
+    echo ${result:1:-1}
+}
+
+# Artifacts associative array aka dictionary
+declare -A artifacts
+
+artifacts["apkeep"]="EFForg/apkeep apkeep-x86_64-unknown-linux-gnu"
+artifacts["apktool.jar"]="iBotPeaches/Apktool apktool .jar"
+
+# Fetch all the dependencies
+for artifact in "${!artifacts[@]}"; do
+    if [ ! -f $artifact ]; then
+        echo "Downloading $artifact"
+        curl -L -o $artifact $(get_artifact_download_url ${artifacts[$artifact]})
+    fi
+done
+
+chmod +x apkeep
