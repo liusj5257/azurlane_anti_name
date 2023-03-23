0b4f4591ecfa523ca649b218a5f67550d3f71716
liusj5257 <87109954+liusj5257@users.noreply.github.com>
2023-03-23 18:04:53 +0800

    Update patch_qudao.sh
    add MUMU

--- patch_qudao.sh
+++ patch_qudao.sh
@@ -28,7 +28,7 @@ chmod +x apkeep
 # Download Azur Lane
 download_azurlane () {
     if [ ! -f "AzurLane.apk" ]; then
-    # 渠道服全都不是直链,不知道怎么搞了
+    # 这个链接是MUMU下载的,应该是9游,其他渠道自行修改直链
     url="https://downali.game.uc.cn/s1/2/10/20230213150150_blhx_uc_2022_11_02_18_24_01.apk?x-oss-process=udf/uc-apk,ZBHDhDR0LVBkTsK*wpLCng==afae37c2a88fd1ca&sh=10&sf=1831727323&vh=18330f93bd450707942ce0b882a0c6b2&cc=2521889677&did=217b729f8a4841dd81901816dfba551f"
     # 使用wget命令下载apk文件
     curl -o blhx.apk  $url
