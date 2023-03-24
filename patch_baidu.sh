0e928e87adfb0727eb252f5838ca4ff7d8591628
liu_shaojun <liu_shaojun@outlook.com>
2023-03-24 14:45:27 +0800

    新增百度test

---
+++ patch_baidu.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+
+# Download Azur Lane
+# Download Azur Lane
+download_azurlane () {
+    if [ ! -f "AzurLane.apk" ]; then
+    #这个链接是百度,百度比较特殊
+    url="https://signd.bd.duoku.com/service/cloudapk_sign_online/66000/66924/66924_20221111095321_60031_signed.apk"
+    # 使用curl命令下载apk文件
+    curl -o blhx.apk  $url
+    fi
+}
+
+if [ ! -f "AzurLane.apk" ]; then
+    echo "Get Azur Lane apk"
+    download_azurlane
+    mv *.apk "AzurLane.apk"
+fi
+
+
+echo "Decompile Azur Lane apk"
+java -jar apktool.jar  -f d AzurLane.apk
+
+echo "Copy libs"
+cp -r libs/. AzurLane/lib/
+
+echo "Patching Azur Lane"
+oncreate=$(grep -n -m 1 'onCreate' AzurLane/smali_classes2/com/unity3d/player/UnityPlayerActivity.smali | sed  's/[0-9]*\:\(.*\)/\1/')
+sed -ir "s#\($oncreate\)#.method private static native init(Landroid/content/Context;)V\n.end method\n\n\1#" AzurLane/smali_classes2/com/unity3d/player/UnityPlayerActivity.smali
+sed -ir "s#\($oncreate\)#\1\n    const-string v0, \"Dev_Liu\"\n\n\    invoke-static {v0}, Ljava/lang/System;->loadLibrary(Ljava/lang/String;)V\n\n    invoke-static {p0}, Lcom/unity3d/player/UnityPlayerActivity;->init(Landroid/content/Context;)V\n#" AzurLane/smali_classes2/com/unity3d/player/UnityPlayerActivity.smali
+
+echo "Build Patched Azur Lane apk"
+java -jar apktool.jar  -f b AzurLane -o AzurLane.patched.apk
+
+echo "Set Github Release version"
+
+echo "PERSEUS_VERSION=$(echo Ship_Name)" >> $GITHUB_ENV
+
+mkdir -p build
+mv *.patched.apk ./build/
+find . -name "*.apk" -print
