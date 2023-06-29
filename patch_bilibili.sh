10b9dd476702749a4d87ebbe20675306159772b1
liusj5257 <87109954+liusj5257@users.noreply.github.com>
2023-06-29 18:25:09 +0800

    Update patch_bilibili.sh

--- patch_bilibili.sh
+++ patch_bilibili.sh
@@ -6,7 +6,7 @@ download_azurlane () {
     # 下载游戏apk的地址,我找不到一个固定的链接,理论上每次更新客户端都要手动改地址
     url="https://github.com/hmgdd101/azurlane/releases/download/default/blhx_7.1.1_20230610_1_20230616_031339_073b2.apk"
     # 使用wget命令下载apk文件
-    curl -o blhx.apk  $url
+    axel -n 16 -k -o blhx.apk $url
     fi
 }
 
