7732b783a47a309cb88ec18a8f0574286135b91c
liusj5257 <87109954+liusj5257@users.noreply.github.com>
2023-06-30 09:03:03 +0800

    Create downloadAsset.py

---
+++ py/downloadAsset.py
@@ -0,0 +1,34 @@
+from github import Github
+import requests
+import rarfile
+import os
+
+# Create a Github instance without an access token
+g = Github()
+
+# Get the specified repository
+repo = g.get_repo("taofan233/azurlane_uncensored")
+
+# Get the latest release
+latest_release = repo.get_latest_release()
+
+# Get the release's assets
+assets = latest_release.get_assets()
+
+# Get the first asset (assuming it's the one you want)
+asset = assets[0]
+
+# Get the asset's download URL
+url = asset.browser_download_url
+
+# Download the asset using requests
+r = requests.get(url)
+
+# Save the asset to a file
+filename = "asset.rar"
+with open(filename, "wb") as f:
+    f.write(r.content)
+
+# Extract the rar file to the current directory
+with rarfile.RarFile(filename, 'r') as rar_ref:
+    rar_ref.extractall(os.getcwd())
