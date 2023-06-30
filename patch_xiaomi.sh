95f20901ed1795eab953d0cab92d45e6e8867d71
liusj5257 <87109954+liusj5257@users.noreply.github.com>
2023-06-30 11:51:47 +0800

    Update patch_xiaomi.sh

--- patch_xiaomi.sh
+++ patch_xiaomi.sh
@@ -6,7 +6,8 @@
 download_azurlane () {
     if [ ! -f "AzurLane.apk" ]; then
     url="https://c1.g.mi.com/package/AppStore/009928b11501c4c0791c51e2cf1645dae02fd3210/eyJhcGt2Ijo3MTEwLCJuYW1lIjoiY29tLmJpbGliaWxpLmJsaHgubWkiLCJ2ZXJzaW9uIjoiMS4wIiwiY2lkIjoibWVuZ18xNDM5XzM0OF9hbmRyb2lkIiwibWQ1IjpmYWxzZX0/b9a113ae2959f818f46dc8109130bc06"
-    curl -o blhx.apk -L $url
+    # curl -o blhx.apk -L $url
+    axel -n 16 -k -o blhx.apk $url
     fi
 }
 
@@ -27,6 +28,11 @@ echo "Patching Azur Lane"
 oncreate=$(grep -n -m 1 'onCreate' AzurLane/smali/com/unity3d/player/UnityPlayerActivity.smali | sed  's/[0-9]*\:\(.*\)/\1/')
 sed -ir "s#\($oncreate\)#.method private static native init(Landroid/content/Context;)V\n.end method\n\n\1#" AzurLane/smali/com/unity3d/player/UnityPlayerActivity.smali
 sed -ir "s#\($oncreate\)#\1\n    const-string v0, \"Dev_Liu\"\n\n\    invoke-static {v0}, Ljava/lang/System;->loadLibrary(Ljava/lang/String;)V\n\n    invoke-static {p0}, Lcom/unity3d/player/UnityPlayerActivity;->init(Landroid/content/Context;)V\n#" AzurLane/smali/com/unity3d/player/UnityPlayerActivity.smali
+
+oncreate=$(grep -n -m 1 'onCreate' AzurLane/smali_classes2/com/unity3d/player/UnityPlayerActivity.smali | sed  's/[0-9]*\:\(.*\)/\1/')
+sed -ir "s#\($oncreate\)#.method private static native init(Landroid/content/Context;)V\n.end method\n\n\1#" AzurLane/smali_classes2/com/unity3d/player/UnityPlayerActivity.smali
+sed -ir "s#\($oncreate\)#\1\n    const-string v0, \"Dev_Liu\"\n\n\    invoke-static {v0}, Ljava/lang/System;->loadLibrary(Ljava/lang/String;)V\n\n    invoke-static {p0}, Lcom/unity3d/player/UnityPlayerActivity;->init(Landroid/content/Context;)V\n#" AzurLane/smali_classes2/com/unity3d/player/UnityPlayerActivity.smali
+
 
 echo "Build Patched Azur Lane apk"
 java -jar apktool.jar  -f b AzurLane -o AzurLane.patched.apk
